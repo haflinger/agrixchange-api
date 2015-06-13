fabcd5c797b2e9ac20f231a78f388ff3b48c9c55
Blump <colinet.julien@hotmail.fr>
2015-06-13 10:35:14 +0000

    api V1

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -27,4 +27,104 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
+
+    /**
+     * @var string Lastname of the person
+     *
+     * @ORM\Column(type="string", nullable=false, length=55)
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=false, length=55)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true, length=55)
+     */
+    private $mobile;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return User
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string 
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
 }
